feac1d7816bfbdd5474724bf9f498beac57108dc
azyd-byte <birulauthijaualam@gmail.com>
2025-03-04 17:28:27 +0700

    props

--- belajar-react-dasar/src/hello-world/HelloWorld.jsx
+++ belajar-react-dasar/src/hello-world/HelloWorld.jsx
@@ -1,8 +1,33 @@
 export default function HelloWorld() {
+  const props = {
+    text: "Hello world!",
+  };
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>Selamat Belajar React JS</p>
+      <HeaderHelloWorld {...props} />
+      <ParagraphHelloWorld />
     </div>
   );
+}
+
+export function HeaderHelloWorld({ text = "Ups, lupa kasih teks" }) {
+  return (
+    <h1
+      style={{
+        color: "red",
+        backgroundColor: "aqua",
+      }}
+    >
+      {text.toUpperCase()}
+    </h1>
+  );
+}
+
+export function ParagraphHelloWorld() {
+  const text = "Selamat Belajar React JS dari programmer zaman now";
+  const style = {
+    color: "blue",
+    backgroundColor: "yellow",
+  };
+  return <p style={style}>{text.toLowerCase()}</p>;
 }
